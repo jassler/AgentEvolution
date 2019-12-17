4a8ee7e28264cc9c05802f3e16c985aa284b947e
jassler <erifetim@gmail.com>
2019-12-17 16:43:20 +0100

    Added join function for containers

--- src/prettyprint.hpp
+++ src/prettyprint.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
 #include <array>
 
 template<typename T, size_t S>
@@ -15,4 +16,32 @@ std::ostream& operator<<(std::ostream& os, const std::array<T, S> arr) {
     }
 
     return os;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::initializer_list<T> list) {
+    if(list.size() == 0) {
+        os << "{ }";
+    } else {
+        auto it = list.begin();
+        os << "{ " << *it;
+        while(++it != list.end())
+            os << ", " << *it ;
+        os << " }";
+    }
+}
+
+namespace pp {
+
+    template<typename TContainer>
+    std::string join(TContainer arr, char sep) {
+        std::stringstream result;
+
+        auto it = arr.begin();
+        result << *it;
+        while(++it != arr.end())
+            result << sep << *it;
+
+        return result.str();
+    }
 }
