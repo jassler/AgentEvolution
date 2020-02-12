b4de8322b517e513fd65cb55fe4f4412e5fac638
jassler <erifetim@gmail.com>
2020-02-12 11:22:57 +0100

    Trying multiple starting agents, currently buggy
    Python seems to add a backslash to quotes, which it shouldn't do argh

--- outcreator.py
+++ outcreator.py
@@ -9,6 +9,8 @@ import termcolor
 from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
 
+
+
 payoffs = [
     "{{0,1,-1},{-1,0,1},{1,-1,0}}",
     # "{{0,1,-1},{1,0,1},{1,-1,0}}",
@@ -16,18 +18,18 @@ payoffs = [
     # "{{0,1,-1},{-1,0,1},{-1,1,0}}",
     # "{{0,1,1},{1,0,1},{1,1,0}}",
     # "{{0,1,1},{-1,0,1},{1,1,0}}",
-    # "{{0,1,-1},{-1,0,1},{-1,1,0}}"
+    "{{0,1,-1},{-1,0,1},{-1,1,0}}"
     # "{{0,-1,1},{-1,0,1},{1,1,0}}",
-    "{{0,1,1},{-1,0,1},{-1,-1,0}}",
-    "{{0,1,1},{1,0,1},{-1,-1,0}}",
+    # "{{0,1,1},{-1,0,1},{-1,-1,0}}",
+    # "{{0,1,1},{1,0,1},{-1,-1,0}}",
     # "{{0,1,-1},{1,0,1},{1,-1,0}}",
     # "{{0,-1,-1},{1,0,1},{-1,1,0}}",
     # "{{0,-1,1},{-1,0,1},{-1,-1,0}}",
     # "{{0,-1,-1},{1,0,-1},{-1,-1,0}}",
     # "{{0,-1,-1},{1,0,-1},{-1,-1,0}}",
     # "{{0,1,-1},{-1,0,1},{-1,1,0}}",
-    "{{0,1,-1},{1,0,-1},{-1,-1,0}}",
-    "{{0,-1,-1},{-1,0,-1},{-1,-1,0}}"
+    # "{{0,1,-1},{1,0,-1},{-1,-1,0}}",
+    # "{{0,-1,-1},{-1,0,-1},{-1,-1,0}}"
     # "{{0,1},{1,0}}",
     # "{{0,1},{-1,0}}",
     # "{{0,-1},{1,0}}",
@@ -41,30 +43,34 @@ payoffs = [
 #     r'{0.25,0.75}',
 #     r'{0.75,0.25}'
 # ]
-agents = [
-    r'{0.333,0.333,0.333}',
-    r'{0.25,0.25,0.5}',
-    r'{0.3,0.2,0.5}',
-    r'{0.75,0.15,0.1}',
+# agents = [
+#     r'{0.333,0.333,0.333}',
+#     r'{0.75,0.15,0.1}',
+#     r'{0.15,0.1,0.75}',
+#     r'{0.1,0.75,0.15}',
 #     r'{0.4,0.1,0.5}',
 #     r'{0.2,0.3,0.5}',
 #     r'{0.15,0.35,0.5}',
 #     r'{0.1,0.4,0.5}',
-    r'{1,0,0}',
-    r'{0,1,0}',
-    r'{0,0,1}'
+#     r'{1,0,0}',
+#     r'{0,1,0}',
+#     r'{0,0,1}',
+#     r'{0,0.5,0.5}',
+#     r'{0.5,0.5,0}',
+#     r'{0.5,0,0.5}'
+# ]
+
+agents = [
+    r'{{1,0,0}},{{0,1,0}},{{0,0,1}}',
+    r'{{1,0,0}},{{0,1,0}},{{0,0,1}},{{0.333,0.333,0.333}}'
 ]
 
 genome_probs = [
-    0,
-    0.0001,
-    0.01,
-    0.25
+    0
 ]
 
 matrix_probs = [
-    0,
-    0.01
+    0
 ]
 
 
@@ -94,12 +100,8 @@ if __name__ == "__main__":
     calls_to_make = []
 
     for payoff, agentstart, genome_prob, matrix_prob in itertools.product(payoffs, agents, genome_probs, matrix_probs):
-        if genome_prob == 0 and matrix_prob == 0:
-            continue
-
         phensize = payoff.count('},{') + 1
-        gensize = agentstart.count(',') + 1
-        agentstart = '{' + agentstart + '}'
+        gensize = agentstart.split('}')[0].count(',') + 1
 
         filename = payoff \
                 .replace("-1", "-") \
@@ -111,7 +113,8 @@ if __name__ == "__main__":
                 .replace(";", ",") + \
             agentstart \
                 .replace('{{', '_') \
-                .replace('}}', '_') + \
+                .replace('}}', '_') \
+                .replace('},{', '-') + \
             '{},{}'.format(genome_prob, matrix_prob)
         
         odir = 'bin'
@@ -119,14 +122,14 @@ if __name__ == "__main__":
         ofile = os.path.join(odir, '{}.out'.format(filename))
 
         arglist = ["make",
-                "POPULATION={}".format(1024),
-                "PAYOFF=\"{}\"".format(payoff),
-                "AGENTSTART=\"{}\"".format(agentstart),
+                "POPULATION={}".format(1212),
+                'PAYOFF="{}"'.format(payoff),
+                'AGENTSTART={}'.format(agentstart),
                 "GENSIZE={}".format(gensize),
                 "PHENSIZE={}".format(phensize),
                 "GENOME_PROB={}".format(genome_prob),
                 "MATRIX_PROB={}".format(matrix_prob),
-                "GENERATIONS={}".format(10000),
+                "GENERATIONS={}".format(1000),
 
                 "LOG_DEBUG={}".format(1),
                 "LOG_PROGRESS={}".format(0),
@@ -137,6 +140,8 @@ if __name__ == "__main__":
         ]
         
         print(termcolor.colored(subprocess.list2cmdline(arglist), 'green', attrs=['bold', 'underline']))
+
+        input("press enter")
         f.write(subprocess.list2cmdline(arglist) + '\n')
         calls_to_make.append([arglist, objdir])
 
@@ -145,5 +150,7 @@ if __name__ == "__main__":
     total = len(calls_to_make)
     pool = ThreadPool(8)
     pool.map(compile, calls_to_make)
+
+    print('done')
 
     
