23cf1824a35f878a89a9dc3ce585c3209b1be438
jassler <erifetim@gmail.com>
2020-02-14 12:26:00 +0100

    Platform independent filesystem (?)

--- src/main.hpp
+++ src/main.hpp
@@ -6,14 +6,17 @@
 #include "fileagent.hpp"
 #include "defines.hpp"
 #include "prettyprint.hpp"
+
+#include "filesystemsupport.hpp"
+
 #include <iostream>
 #include <initializer_list>
-#include <filesystem>
 #include <algorithm>
 #include <sstream>
 #include <chrono>
 #include <vector>
 #include <locale>
+#include <string.h>
 
 void calculate_averages(std::vector<std::string> filenames, std::string outfilename, std::initializer_list<std::string> headers, char sep = ';');
 
